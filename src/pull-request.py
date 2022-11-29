f417c10dfa6389a7a81269d3c57de41649dc602e
Keith Wang <keithwang@Keiths-MacBook-Air.local>
2022-11-29 16:31:45 -0500

    refactor

--- src/pull-request.py
+++ src/pull-request.py
@@ -3,21 +3,36 @@ import json
 import base64
 import os
 
-def create_branch(username, repo, newbranch, token):
-    headers = {'Authorization': "Token " + token}
-    url = "https://api.github.com/repos/"+username+"/"+repo+"/git/refs/heads"
-    branches = requests.get(url, headers=headers).json()
+github_url = "https://api.github.com"
+org = "kxw9298"
+github_token = os.getenv('GITHUB_TOKEN')
+headers = {'Authorization': f"Token {github_token}"}
+
+
+def create_branch(target):
+    repo_name = target.get("repo_name")
+    branch_name = target.get("branch_name")
+    url = f"{github_url}/repos/{org}/{repo_name}/git/refs"
+    branches = requests.get(f"{url}/heads", headers=headers).json()
     branch, sha = branches[-1]['ref'], branches[-1]['object']['sha']
-    res = requests.post('https://api.github.com/repos/'+username+'/'+repo+'/git/refs', json={
-        "ref": "refs/heads/"+newbranch,
-        "sha": sha
-    }, headers=headers)
-    print(res.content)
+    payload = {"ref": f"refs/heads/{branch_name}",
+               "sha": sha}
+    r = requests.post(url, json=payload, headers=headers)
+    print(r.content)
 
 
-token = os.getenv('GITHUB_TOKEN')
-username = "kxw9298"
-repo = "git-python"
-newbranch="test"
+def read_github_file_and_update_content(target):
+    print("")
+    username = target.get("username")
+    repo = target.get("repo")
+    newbranch = target.get("newbranch")
+    token = target.get("token")
+    headers = {'Authorization': "Token " + token}
 
-create_branch(username, repo, newbranch, token)
+
+target_create_branch = {
+    "repo_name": "git-python",
+    "branch_name": "test"}
+
+
+create_branch(target_create_branch)
