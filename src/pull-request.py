04db8c6877ff4f657ac436ab4afbbd109fa581f5
Keith Wang <keithwang@Keiths-MacBook-Air.local>
2022-11-29 16:50:54 -0500

    refactor

--- src/pull-request.py
+++ src/pull-request.py
@@ -22,17 +22,35 @@ def create_branch(target):
 
 
 def read_github_file_and_update_content(target):
-    print("")
-    username = target.get("username")
-    repo = target.get("repo")
-    newbranch = target.get("newbranch")
-    token = target.get("token")
-    headers = {'Authorization': "Token " + token}
+    repo_name = target.get("repo_name")
+    branch_name = target.get("branch_name")
+    file_path = target.get("file_path")
+    url = f"{github_url}/repos/{org}/{repo_name}/contents/{file_path}"
+    r = requests.get(
+        url,
+        params={"ref": branch_name},
+        headers=headers
+    )
+    data = r.json()
+    file_content = data['content']
+    file_content_encoding = data.get('encoding')
+    if file_content_encoding == 'base64':
+        file_content = base64.b64decode(file_content).decode()
+    print(file_content)
+    return {
+        "file_contents": file_content
+    }
 
 
 target_create_branch = {
     "repo_name": "git-python",
     "branch_name": "test"}
 
+target_read_file_and_update = {
+    "repo_name": "git-python",
+    "branch_name": "test",
+    "file_path": "src/sample/application.properties"
+}
 
-create_branch(target_create_branch)
+# create_branch(target_create_branch)
+read_github_file_and_update_content(target_read_file_and_update)
